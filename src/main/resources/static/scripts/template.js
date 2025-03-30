d7b9db28034eb2d6e3b2b202e02a6b2547d81ed2
ADar <adalvarador@unah.hn>
2025-03-29 18:10:13 -0600

    Correcion de vistat v_Producto y Repositorio Producto.

--- src/main/resources/static/scripts/template.js
+++ src/main/resources/static/scripts/template.js
@@ -1,15 +1,15 @@
 window.addEventListener("DOMContentLoaded", async () => {
-    const roleH1 = document.querySelector("#title");
+    const roleH5 = document.querySelector("#role");
     const optionsNav = document.querySelector("#nav-options");
     
     let userData = JSON.parse(localStorage.getItem("userData"));
 
-    roleH1.textContent = userData.rol.nombreRol;
+    roleH5.textContent = userData.rol.nombreRol;
     
     userData.rol.permisos.forEach((p) => {
         if(p.acceso && p.permiso.accesoDirecto) {
             const option = document.createElement("a");
-            option.classList.add("btn");
+            option.classList.add("nav");
             option.textContent = p.permiso.descripcion;
             option.dataset.id = p.permiso.idPermiso;
 
